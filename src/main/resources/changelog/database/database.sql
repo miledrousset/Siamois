7a66e1b7a14fb424cfbf5a1fb231d496f946f2f8
Julien Linget <julien.linget@mom.fr>
2024-10-09 14:19:20 +0200

    setup/#4: Ajout du premier brouillon du SQL de la BDD

--- src/main/resources/changelog/database/database.sql
+++ src/main/resources/changelog/database/database.sql
@@ -14,82 +14,103 @@
 --    - name (nom) : texte libre
 --  - label (libellé) : vocabulaire contrôlé
 
-CREATE TABLE type_unite_enregistrement (
+CREATE TABLE type_unite_enregistrement
+(
 
-  type_unite_enregistrement_id INT NOT NULL,
-  type_unite_enregistrement_label VARCHAR(255) NOT NULL,
+    type_ue_id    SERIAL,
+    type_ue_label VARCHAR(255) NOT NULL,
 
-  PRIMARY KEY (type_unite_enregistrement_id)
-
-);
-
-CREATE TABLE unite_enregistrement (
-
-  unite_enregistrement_id INT NOT NULL, -- todo : on utilise quoi comme ID ??
-  id_interne VARCHAR(255) NOT NULL,
-  type_unite_enregistrement_id INT,
-  ue_date TIMESTAMP,
-  PRIMARY KEY (unite_enregistrement_id),
-  FOREIGN KEY (type_unite_enregistrement_id) REFERENCES type_unite_enregistrement (type_unite_enregistrement_id)
+    PRIMARY KEY (type_ue_id)
 
 );
 
-CREATE TABLE stratigraphie (
-    unite_enregistrement_id_1 INT, -- la combinaison des deux ue est elle unique ?
-    unite_enregistrement_id_2 INT,
-    type_relation INT -- todo : table séparé?
-    stratigraphie_id INT NOT NULL,
+CREATE TABLE unite_enregistrement
+(
 
-    PRIMARY KEY (stratigraphie_id),
-    FOREIGN KEY (unite_enregistrement_id_1) REFERENCES unite_enregistrement (unite_enregistrement_id)
-    FOREIGN KEY (unite_enregistrement_id_2) REFERENCES unite_enregistrement (unite_enregistrement_id)
+    ue_id      SERIAL,
+    ue_uri     VARCHAR(255) NOT NULL,
+    ue_type_id INT,
+    ue_date    TIMESTAMP,
+    PRIMARY KEY (ue_id),
+    FOREIGN KEY (ue_type_id) REFERENCES type_unite_enregistrement (type_ue_id)
 
-)
+);
+
+CREATE TABLE stratigraphie
+(
+    stratigraphie_ue_id_1 INT, -- la combinaison des deux ue est elle unique ?
+    stratigraphie_ue_id_2 INT,
+    type_relation         INT, -- todo : table séparé?
+
+    PRIMARY KEY (stratigraphie_ue_id_1, stratigraphie_ue_id_2),
+    FOREIGN KEY (stratigraphie_ue_id_1) REFERENCES unite_enregistrement (ue_id),
+    FOREIGN KEY (stratigraphie_ue_id_2) REFERENCES unite_enregistrement (ue_id)
+
+);
 
 -- todo INSERT enumeration of values for record unit type
 
-CREATE TABLE equipe (
-    equipe_id INT NOT NULL ,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    date_debut TIMESTAMP,
-    date_fin TIMESTAMP,
+CREATE TABLE equipe
+(
+    equipe_id          SERIAL,
+    equipe_name        VARCHAR(255) NOT NULL,
+    equipe_description VARCHAR(255) NOT NULL,
+    equipe_date_debut  TIMESTAMP,
+    equipe_date_fin    TIMESTAMP,
 
     PRIMARY KEY (equipe_id)
 );
 
-CREATE TABLE type_role (
+CREATE TABLE type_role
+(
 
-  type_role_id INT NOT NULL ,
-  type_role_label VARCHAR(255) NOT NULL,
+    type_role_id    SERIAL,
+    type_role_label VARCHAR(255) NOT NULL,
 
-  PRIMARY KEY (type_role_id)
+    PRIMARY KEY (type_role_id)
 );-- todo INSERT enumeration of values for type role
 
-CREATE TABLE role (
-    role_id INT NOT NULL ,
-    type_role_id INT NOT NULL,
-    role_scope INT, -- todo : C'est quoi?
-    role_date_debut DATE,
-    role_date_fin DATE,
-    equipe_id INT,
-    droit_access_role INT, -- todo : fk vers quoi? INT ?
+CREATE TABLE role
+(
+    role_id           SERIAL,
+    role_type_id      INT NOT NULL,
+    role_scope        INT,                                      -- todo : C'est quoi?
+    role_date_debut   TIMESTAMP,
+    role_date_fin     TIMESTAMP,
+    role_equipe_id    INT,
+    role_droit_access INT,                                      -- todo : fk vers quoi? INT ?
 
     PRIMARY KEY (role_id),
-    FOREIGN KEY (equipe_id) REFERENCES equipe (equipe_id), -- todo : verifier ce lien, pas sur d'avoir compris (GB)
-    FOREIGN KEY (type_role_id) REFERENCES type_role (type_role_id)
-)
-
-CREATE TABLE auteur_role (
-
-  role_id INT NOT NULL,
-  auteur_id  INT NOT NULL,
-  -- todo : finir
+    FOREIGN KEY (role_equipe_id) REFERENCES equipe (equipe_id), -- todo : verifier ce lien, pas sur d'avoir compris (GB)
+    FOREIGN KEY (role_type_id) REFERENCES type_role (type_role_id)
 );
 
+CREATE TABLE auteur
+(
+    auteur_id          SERIAL,
+    auteur_nom         VARCHAR,
+    auteur_mail        VARCHAR,
+
+    auteur_role        VARCHAR,
+    auteur_droit_acces VARCHAR,
+
+    PRIMARY KEY (auteur_id)
+);
+
+CREATE TABLE auteur_role
+(
+
+    role_id   INT NOT NULL,
+    auteur_id INT NOT NULL,
+
+    PRIMARY KEY (auteur_id, role_id),
+    FOREIGN KEY (role_id) REFERENCES role (role_id),
+    FOREIGN KEY (auteur_id) REFERENCES auteur (auteur_id)
+);
+
+/*
 CREATE TABLE operation_archeo ( -- c'est quoi??
     operation_archeo_id INT NOT NULL ,
-
     PRIMARY KEY (operation_archeo_id)
 );
 
@@ -100,33 +121,148 @@ CREATE TABLE systeme_coordo (
 
     PRIMARY KEY (systeme_coordo)
 )
+*/
 
-CREATE TABLE intervention (
-    intervention_id INT NOT NULL ,
-    operation_archeo_id INT,
-    name VARCHAR(255),
-    description TEXT,
-    type_intervention_label INT, -- todo : pas du tout sur (gb)
-    annee_intervention INT,
-    parent_intervention_id INT,
-    coordonnee_intervention_id INT, -- si pas de SIG
+CREATE TABLE intervention
+(
+    intervention_id               SERIAL,
+    intervention_operation_archeo INT,
+    intervention_nom              VARCHAR(255),
+    intervention_description      TEXT,
+    -- type_intervention_label INT, -- todo : pas du tout sur (gb)
+    intervention_annee            INT,
+    intervention_parent_id        INT,
+    intervention_coordo           INT, -- si pas de SIG
     -- todo : systeme_coordo stockage spatial
-    date_debut_intervention TIMESTAMP,
-    date_fin_intervention TIMESTAMP
+    intervention_date_debut       TIMESTAMP,
+    intervention_date_fin         TIMESTAMP,
 
     PRIMARY KEY (intervention_id),
-    FOREIGN KEY (operation_archeo_id) REFERENCES operation_archeo (operation_archeo_id)
-    FOREIGN KEY (parent_intervention_id) REFERENCES intervention (intervention_id)
-    FOREIGN KEY (coordonnee_intervention) REFERENCES systeme_coordo (systeme_coordo_id)
+    -- FOREIGN KEY (operation_archeo_id) REFERENCES operation_archeo (operation_archeo_id),
+    FOREIGN KEY (intervention_parent_id) REFERENCES intervention (intervention_id)
+    -- FOREIGN KEY (coordonnee_intervention) REFERENCES systeme_coordo (systeme_coordo_id)
 );
 
-CREATE TABLE intervention_equipe (
-    -- pour
-    intervention_id  INT NOT NULL,
-    equipe_id  INT NOT NULL,
-    -- todo : finir
+CREATE TABLE intervention_equipe
+(
+    intervention_id INT NOT NULL,
+    equipe_id       INT NOT NULL,
+
+
+
+    PRIMARY KEY (intervention_id, equipe_id),
+    FOREIGN KEY (intervention_id) REFERENCES intervention (intervention_id),
+    FOREIGN KEY (equipe_id) REFERENCES equipe (equipe_id)
+);
+
+CREATE TABLE etude_ue_g2
+(
+    etude_ue_id        SERIAL,
+    etude_ue_auteur    VARCHAR,
+    etude_ue_date      TIMESTAMP,
+
+    etude_ue_typologie VARCHAR,
+    etude_ue_document  VARCHAR,
+
+    PRIMARY KEY (etude_ue_id)
+    -- Open form et Open Typo
+);
+
+CREATE TABLE interpretation_etude_ue (
+    interpretation_etude_ue INT,
+    interpreration_ue INT,
+
+    PRIMARY KEY (interpreration_ue, interpretation_etude_ue),
+    FOREIGN KEY (interpreration_ue) REFERENCES unite_enregistrement(ue_id),
+    FOREIGN KEY (interpretation_etude_ue) REFERENCES etude_ue_g2(etude_ue_id)
+);
+
+CREATE TABLE document_g3
+(
+    document_id           SERIAL PRIMARY KEY,
+    document_nature_id    INT,
+    document_echelle_id   INT,
+    document_auteur_id    INT,
+    document_format_id    INT,
+    document_parent_id    INT DEFAULT NULL,
+    document_localisation VARCHAR,
+    -- TODO
+
+    FOREIGN KEY (document_parent_id) REFERENCES document_g3 (document_id)
+);
+
+CREATE TABLE etude_prelevement_g2
+(
+    etude_prelevement_id               SERIAL,
+    etude_prelevement_auteur_id        INT,
+    etude_prelevement_date_etude       TIMESTAMP,
+    etude_prelevement_methode_etude_id INT,
+    etude_prelevement_typologie_id     INT,
+    etude_prelevement_document_id      INT,
+
+    PRIMARY KEY (etude_prelevement_id)
+);
+
+CREATE TABLE mobilier_g3
+(
+    mobilier_id           SERIAL PRIMARY KEY,
+    mobilier_localisation VARCHAR,
+    mobilier_media        VARCHAR,
+    mobilier_stockage     VARCHAR
+);
+
+CREATE TABLE categorie_prelevement
+(
+    categorie_prelevement_id  SERIAL PRIMARY KEY,
+    categorie_prelevement_nom VARCHAR
+);
+
+CREATE TYPE operation_traitement_type_label_enum AS ENUM ('Consolidation', 'Radiographie', 'Nettoyage', 'Restauration');
+CREATE TABLE operation_traitement
+(
+    operation_traitement_id              SERIAL,
+    operation_traitement_type_label      operation_traitement_type_label_enum,
+    operation_traitement_type_definition TEXT,
+
+    PRIMARY KEY (operation_traitement_id)
 );
 
 
+CREATE TABLE mouvement_prelevement
+(
+    mouvement_prelevement_id             SERIAL,
+    mouvement_prelevement_date_sortie    TIMESTAMP,
+    mouvement_prelevement_date_retour    TIMESTAMP,
+    mouvement_prelevement_lieu           TEXT, -- table de lieu ?
+    mouvement_prelevement_prelevement_id INT,
+
+    PRIMARY KEY (mouvement_prelevement_id)
+);
+
+CREATE TABLE prelevement
+(
+    prelevement_id                    SERIAL,
+    prelevement_ue_id                 INT,
+    prelevement_document_ID           INT,
+    prelevement_morphometrie          VARCHAR,
+    prelevement_categorie_id          INT,
+    prelevement_traitement_id         INT,
+    prelevement_etude_id              INT,
+    prelevement_methode_collecte      VARCHAR DEFAULT 'Fouille',
+    prelevement_date_collecte         TIMESTAMP,
+    prelevement_localisation_collecte VARCHAR,
+    prelevement_stockage              VARCHAR,
+    prelevement_mouvement_id               INT,
+
+    PRIMARY KEY (prelevement_id),
+    FOREIGN KEY (prelevement_ue_id) REFERENCES unite_enregistrement (ue_id),
+    FOREIGN KEY (prelevement_document_ID) REFERENCES document_g3 (document_id),
+    FOREIGN KEY (prelevement_categorie_id) REFERENCES categorie_prelevement (categorie_prelevement_id),
+    FOREIGN KEY (prelevement_etude_id) REFERENCES etude_prelevement_g2 (etude_prelevement_id),
+    FOREIGN KEY (prelevement_mouvement_id) REFERENCES mouvement_prelevement(mouvement_prelevement_id)
+);
+
+ALTER TABLE mouvement_prelevement
+ADD FOREIGN KEY (mouvement_prelevement_prelevement_id) REFERENCES prelevement (prelevement_id)
 
 
