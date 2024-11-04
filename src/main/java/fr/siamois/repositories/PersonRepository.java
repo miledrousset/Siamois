8a5c2209fdf78dc8ecc076ed7a9030c1599412bd
Julien Linget <julien.linget@mom.fr>
2024-11-04 10:25:33 +0100

    fix/#23: Add feature/#23: Add missing query for system_role_user

--- src/main/java/fr/siamois/repositories/PersonRepository.java
+++ src/main/java/fr/siamois/repositories/PersonRepository.java
@@ -1,6 +1,7 @@
 package fr.siamois.repositories;
 
 import fr.siamois.models.Person;
+import fr.siamois.models.SystemRole;
 import fr.siamois.models.Team;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -17,5 +18,11 @@ public interface PersonRepository extends CrudRepository<Person, Integer> {
             value = "SELECT * FROM person_role_team prt JOIN person p ON prt.fk_person_id = p.person_id WHERE prt.fk_team_id = :team"
     )
     List<Person> findPersonsOfTeam(@Param("team") Team team);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM system_role sr JOIN public.system_role_user sru on sr.system_role_id = sru.role_id WHERE sru.role_id = :systemRole"
+    )
+    List<Person> findPersonsWithSystemRole(@Param("systemRole") SystemRole systemRole);
 
 }
