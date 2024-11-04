d64b58a1c5ae3c6f9a85b5b404e2e147da110f68
Julien Linget <julien.linget@mom.fr>
2024-11-04 10:31:40 +0100

    feature/#23: Add query for person_role_team

--- src/main/java/fr/siamois/repositories/PersonRepository.java
+++ src/main/java/fr/siamois/repositories/PersonRepository.java
@@ -1,5 +1,6 @@
 package fr.siamois.repositories;
 
+import fr.siamois.models.Concept;
 import fr.siamois.models.Person;
 import fr.siamois.models.SystemRole;
 import fr.siamois.models.Team;
@@ -24,5 +25,20 @@ public interface PersonRepository extends CrudRepository<Person, Integer> {
             value = "SELECT * FROM system_role sr JOIN public.system_role_user sru on sr.system_role_id = sru.role_id WHERE sru.role_id = :systemRole"
     )
     List<Person> findPersonsWithSystemRole(@Param("systemRole") SystemRole systemRole);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM person_role_team prt JOIN person p ON prt.fk_person_id = p.person_id WHERE prt.fk_team_id = :team AND prt.fk_role_concept_id = :role"
+    )
+    List<Person> findPersonsWithRole(@Param("role") Concept role);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM person_role_team prt " +
+                    "JOIN person p ON prt.fk_person_id = p.person_id " +
+                    "WHERE prt.fk_team_id = :team " +
+                    "AND prt.fk_role_concept_id = :role"
+    )
+    List<Person> findPersonsOfTeamWithRole(@Param("team") Team team, @Param("role") Concept role);
 
 }
