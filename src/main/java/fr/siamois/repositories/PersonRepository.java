fadfd7edfcc3da4b651cdf945d06afab5e4f7da1
Julien Linget <julien.linget@mom.fr>
2024-11-04 10:36:15 +0100

    fix/#23: Fix query of PersonRepository.java

--- src/main/java/fr/siamois/repositories/PersonRepository.java
+++ src/main/java/fr/siamois/repositories/PersonRepository.java
@@ -16,25 +16,25 @@ public interface PersonRepository extends CrudRepository<Person, Integer> {
 
     @Query(
             nativeQuery = true,
-            value = "SELECT * FROM person_role_team prt JOIN person p ON prt.fk_person_id = p.person_id WHERE prt.fk_team_id = :team"
+            value = "SELECT p.* FROM person_role_team prt JOIN person p ON prt.fk_person_id = p.person_id WHERE prt.fk_team_id = :team"
     )
     List<Person> findPersonsOfTeam(@Param("team") Team team);
 
     @Query(
             nativeQuery = true,
-            value = "SELECT * FROM system_role sr JOIN public.system_role_user sru on sr.system_role_id = sru.role_id WHERE sru.role_id = :systemRole"
+            value = "SELECT p.* FROM person p JOIN system_role_user sru ON p.person_id = sru.person_id WHERE role_id = :systemRole"
     )
     List<Person> findPersonsWithSystemRole(@Param("systemRole") SystemRole systemRole);
 
     @Query(
             nativeQuery = true,
-            value = "SELECT * FROM person_role_team prt JOIN person p ON prt.fk_person_id = p.person_id WHERE prt.fk_team_id = :team AND prt.fk_role_concept_id = :role"
+            value = "SELECT p.* FROM person_role_team prt JOIN person p ON prt.fk_person_id = p.person_id WHERE prt.fk_team_id = :team AND prt.fk_role_concept_id = :role"
     )
     List<Person> findPersonsWithRole(@Param("role") Concept role);
 
     @Query(
             nativeQuery = true,
-            value = "SELECT * FROM person_role_team prt " +
+            value = "SELECT p.* FROM person_role_team prt " +
                     "JOIN person p ON prt.fk_person_id = p.person_id " +
                     "WHERE prt.fk_team_id = :team " +
                     "AND prt.fk_role_concept_id = :role"
