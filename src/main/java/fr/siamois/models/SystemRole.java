d0e4f095889906904c9eb756a8adeff0b12c7d97
Julien Linget <julien.linget@mom.fr>
2024-11-04 09:36:56 +0100

    feature/#23: Add SystemRole and SystemRoleRepository

---
+++ src/main/java/fr/siamois/models/SystemRole.java
@@ -0,0 +1,24 @@
+package fr.siamois.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "system_role", schema = "public", uniqueConstraints = {
+        @UniqueConstraint(name = "system_role_role_name_key", columnNames = {"role_name"})
+})
+public class SystemRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "system_role_id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "role_name", nullable = false, length = Integer.MAX_VALUE)
+    private String roleName;
+
+}
---
+++ src/main/java/fr/siamois/repositories/SystemRoleRepository.java
@@ -0,0 +1,19 @@
+package fr.siamois.repositories;
+
+import fr.siamois.models.Person;
+import fr.siamois.models.SystemRole;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SystemRoleRepository extends CrudRepository<SystemRole, Integer> {
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM system_role_user sru JOIN system_role sr on sr.system_role_id = sru.role_id WHERE sru.person_id = :person"
+    )
+    List<SystemRole> findSystemRolesOfPerson(@Param("person") Person person);
+
+}
