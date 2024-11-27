dcd59fff2a963bc5cce4b7ba26b624ff42998b90
Julien Linget <julien.linget@mom.fr>
2024-11-27 15:48:12 +0100

    doc/#38: Comments on SpatialUnitFieldBean.java

--- src/main/java/fr/siamois/bean/Field/SpatialUnitFieldBean.java
+++ src/main/java/fr/siamois/bean/Field/SpatialUnitFieldBean.java
@@ -60,6 +60,10 @@ public class SpatialUnitFieldBean implements Serializable {
         this.fieldConfigurationService = fieldConfigurationService;
     }
 
+    /**
+     * Called on page rendering.
+     * Reset all fields.
+     */
     public void init() {
         refSpatialUnits = fieldService.fetchAllSpatialUnits();
         labels = refSpatialUnits.stream()
@@ -72,6 +76,12 @@ public class SpatialUnitFieldBean implements Serializable {
         fParentsSpatialUnits = new ArrayList<>();
     }
 
+    /**
+     * Save the spatial unit in the database.
+     * Display a message if the spatial unit already exists.
+     * Display a message if the spatial unit has been created.
+     * @throws IllegalStateException if the collections are not defined
+     */
     public void save() {
         ConceptFieldDTO selectedConceptFieldDTO = getSelectedConceptFieldDTO().orElseThrow(() -> new IllegalStateException("No concept selected"));
 
@@ -93,6 +103,11 @@ public class SpatialUnitFieldBean implements Serializable {
         }
     }
 
+    /**
+     * Fetch the autocomplete results on API for the category field and add them to the list of concepts.
+     * @param input the input of the user
+     * @return the list of concepts that match the input to display in the autocomplete
+     */
     public List<String> completeCategory(String input) {
         Person person = AuthenticatedUserUtils.getAuthenticatedUser().orElseThrow(() -> new IllegalStateException("User should be connected"));
 
@@ -122,6 +137,10 @@ public class SpatialUnitFieldBean implements Serializable {
         }
     }
 
+    /**
+     * Find the concept selected by the user.
+     * @return the concept selected by the user
+     */
     private Optional<ConceptFieldDTO> getSelectedConceptFieldDTO() {
         return concepts.stream()
                 .filter(conceptFieldDTO -> conceptFieldDTO.getLabel().equalsIgnoreCase(fCategory))
