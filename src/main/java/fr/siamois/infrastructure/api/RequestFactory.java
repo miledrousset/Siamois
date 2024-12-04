fd045b7ee8bc2bdca4e6c5cc3bee5eac505fe96a
Julien Linget <julien.linget@mom.fr>
2024-12-04 11:05:09 +0100

    feat: Add RequestFactory

---
+++ src/main/java/fr/siamois/infrastructure/api/RequestFactory.java
@@ -0,0 +1,37 @@
+package fr.siamois.infrastructure.api;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+/**
+ * Factory to build a RestTemplate with a custom request factory.
+ */
+@Service
+public class RequestFactory {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    public RequestFactory(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
+    /**
+     * Build a RestTemplate with the follow redirect option enabled.
+     * @return The RestTemplate
+     */
+    public RestTemplate buildRestTemplate() {
+        return restTemplateBuilder.requestFactory(() -> new SimpleClientHttpRequestFactory() {
+            @Override
+            protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
+                super.prepareConnection(connection, httpMethod);
+                connection.setInstanceFollowRedirects(true);
+            }
+        }).build();
+    }
+
+}
