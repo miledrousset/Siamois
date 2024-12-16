72ae6052b19db6c39408a4c55d18b8f22724357a
Julien Linget <julien.linget@mom.fr>
2024-12-16 11:22:36 +0100

    feature/#44: Update SessionSettings.java to allow tomcat session persistance

--- src/main/java/fr/siamois/bean/SessionSettings.java
+++ src/main/java/fr/siamois/bean/SessionSettings.java
@@ -3,21 +3,24 @@ package fr.siamois.bean;
 import fr.siamois.models.Team;
 import fr.siamois.models.auth.Person;
 import fr.siamois.models.exceptions.NoTeamSelectedException;
+import fr.siamois.utils.AuthenticatedUserUtils;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import javax.faces.bean.SessionScoped;
 
-@Component
 @Setter
+@Getter
+@Component
 @SessionScoped
 public class SessionSettings {
 
-    @Getter
-    private Person authenticatedUser;
-
     private Team selectedTeam;
+
+    public Person getAuthenticatedUser() {
+        return AuthenticatedUserUtils.getAuthenticatedUser().orElseThrow(() -> new RuntimeException("No authenticated user"));
+    }
 
     public Team getSelectedTeam() throws NoTeamSelectedException {
         if (selectedTeam == null) {
