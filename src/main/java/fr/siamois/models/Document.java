2c26c9cb838f0fdb3088e266800f3e31f90704c5
Julien Linget <julien.linget@mom.fr>
2025-01-14 15:02:31 +0100

    refactor: Remove unused import

--- src/main/java/fr/siamois/models/Document.java
+++ src/main/java/fr/siamois/models/Document.java
@@ -1,10 +1,6 @@
 package fr.siamois.models;
 
-import fr.siamois.models.ark.Ark;
-import fr.siamois.models.auth.Person;
-import fr.siamois.models.vocabulary.Concept;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
