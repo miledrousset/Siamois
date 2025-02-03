b5a76154bbdce06be2bdd378dddafd05a2e1767f
Julien Linget <julien.linget@mom.fr>
2025-02-03 14:23:38 +0100

    fix: Session not selected bug

--- src/main/java/fr/siamois/bean/SessionSettings.java
+++ src/main/java/fr/siamois/bean/SessionSettings.java
@@ -2,13 +2,14 @@ package fr.siamois.bean;
 
 import fr.siamois.models.Institution;
 import fr.siamois.models.auth.Person;
-import fr.siamois.models.exceptions.NoInstitutionSelectedException;
+import fr.siamois.services.InstitutionService;
 import fr.siamois.utils.AuthenticatedUserUtils;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import javax.faces.bean.SessionScoped;
+import java.util.List;
 
 @Setter
 @Getter
@@ -16,7 +17,13 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class SessionSettings {
 
+    private final InstitutionService institutionService;
     private Institution selectedInstitution;
+    private List<Institution> referencedInstitutions;
+
+    public SessionSettings(InstitutionService institutionService) {
+        this.institutionService = institutionService;
+    }
 
     public Person getAuthenticatedUser() {
         return AuthenticatedUserUtils.getAuthenticatedUser().orElseThrow(() -> new IllegalStateException("No authenticated user but user should be authenticated"));
@@ -24,9 +31,31 @@ public class SessionSettings {
 
     public Institution getSelectedInstitution() {
         if (selectedInstitution == null) {
-            throw new NoInstitutionSelectedException("No institution selected for the current session");
+            setupSession();
         }
         return selectedInstitution;
+    }
+
+    public List<Institution> getReferencedInstitutions() {
+        if (referencedInstitutions == null) {
+            setupSession();
+        }
+        return referencedInstitutions;
+    }
+
+    public void setupSession() {
+        Person authUser = getAuthenticatedUser();
+        List<Institution> result;
+        if (authUser.hasRole("ADMIN")) {
+            result = institutionService.findAll();
+        } else {
+            result = institutionService.findInstitutionsOfPerson(authUser);
+        }
+
+        result.sort(((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName())));
+
+        selectedInstitution = result.get(0);
+        referencedInstitutions = result;
     }
 
 }
--- src/main/java/fr/siamois/config/handler/LoginSuccessHandler.java
+++ src/main/java/fr/siamois/config/handler/LoginSuccessHandler.java
@@ -1,9 +1,6 @@
 package fr.siamois.config.handler;
 
-import fr.siamois.bean.NavBean;
 import fr.siamois.bean.SessionSettings;
-import fr.siamois.models.auth.Person;
-import fr.siamois.services.InstitutionService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -25,13 +22,9 @@ import java.io.IOException;
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final SessionSettings sessionSettings;
-    private final NavBean navBean;
-    private final InstitutionService institutionService;
 
-    public LoginSuccessHandler(SessionSettings sessionSettings, NavBean navBean, InstitutionService institutionService) {
+    public LoginSuccessHandler(SessionSettings sessionSettings) {
         this.sessionSettings = sessionSettings;
-        this.navBean = navBean;
-        this.institutionService = institutionService;
     }
 
     /**
@@ -43,7 +36,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
      */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        setupSession();
+        sessionSettings.setupSession();
         redirectRequest(request, response);
     }
 
@@ -62,23 +55,5 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
         } else {
             response.sendRedirect(contextPath + "/");
         }
-    }
-
-    private void setupSession() {
-        if (sessionSettings.getAuthenticatedUser().hasRole("ADMIN")) {
-            navBean.setInstitutions(institutionService.findAll());
-        } else {
-            Person authUser = sessionSettings.getAuthenticatedUser();
-            navBean.setInstitutions(institutionService.findInstitutionsOfPerson(authUser));
-        }
-
-        navBean.getInstitutions()
-                .sort(((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName())));
-
-        if (!navBean.getInstitutions().isEmpty()) {
-            navBean.setSelectedInstitution(navBean.getInstitutions().get(0));
-        }
-
-        log.info("User {} logged in", sessionSettings.getAuthenticatedUser().getUsername());
     }
 }
