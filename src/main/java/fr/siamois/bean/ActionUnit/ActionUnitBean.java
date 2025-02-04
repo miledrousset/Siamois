a6cd6e71d170aed1c580049c4d717bdf6f018d16
Grégory Bliault <gregorybliault@gmail.com>
2025-02-04 11:50:09 +0100

    WIP action code editor

--- src/main/java/fr/siamois/bean/ActionUnit/ActionUnitBean.java
+++ src/main/java/fr/siamois/bean/ActionUnit/ActionUnitBean.java
@@ -18,7 +18,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import jakarta.faces.context.FacesContext;
 
 @Slf4j
 @Data
@@ -37,6 +37,12 @@ public class ActionUnitBean implements Serializable {
     // Field related
     private Boolean editType;
     private ConceptFieldDTO fType;
+
+    // todo: remove below and implement properly
+    private Concept c1 = new Concept();
+    private Concept c2 = new Concept();
+    private List<Concept> actionCodeTypeOptions ;
+    private List<ActionCode> secondaryActionCodes ;
 
 
     public ActionUnitBean(ActionUnitService actionUnitService) {
@@ -67,31 +73,53 @@ public class ActionUnitBean implements Serializable {
 
     }
 
+    public void handleSelectPrimaryCode() {
+        return;
+    }
+
+    public void addNewSecondaryCode() {
+        ActionCode code = new ActionCode();
+        Concept c = new Concept();
+        c.setLabel("Code OA");
+        code.setCode("1115613zz");
+        code.setType(c);
+        secondaryActionCodes.add(code);
+    }
 
     public void init() {
 
-        // reinit
-        actionUnitErrorMessage = null;
-        actionUnit = null;
+        if (!FacesContext.getCurrentInstance().isPostback()) {
+            // reinit
+            actionUnitErrorMessage = null;
+            actionUnit = null;
 
-       // Get the requested action from DB
-        try {
-            if(id!=null) {
-                actionUnit = actionUnitService.findById(id);
-                Concept typeConcept = actionUnit.getType();
-                fType = new ConceptFieldDTO();
-                fType.setLabel(typeConcept.getLabel());
-                // If thesaurus we can reconstruct the DTO
-                fType.setUri(typeConcept.getVocabulary().getBaseUri()+"?idc="+typeConcept.getExternalId()+"&idt="+typeConcept.getVocabulary().getExternalVocabularyId());
+            c1.setLabel("Code OA");
+            c2.setLabel("Code OP");
+            actionCodeTypeOptions = List.of(c1,c2);
+            // Get the requested action from DB
+            try {
+                if(id!=null) {
+                    actionUnit = actionUnitService.findById(id);
+                    ActionCode primaryActionCode = new ActionCode();
+                    actionUnit.setPrimaryActionCode(primaryActionCode);
+                    secondaryActionCodes = new ArrayList<>();
+                    Concept typeConcept = actionUnit.getType();
+                    fType = new ConceptFieldDTO();
+                    fType.setLabel(typeConcept.getLabel());
+                    // If thesaurus we can reconstruct the DTO
+                    fType.setUri(typeConcept.getVocabulary().getBaseUri()+"?idc="+typeConcept.getExternalId()+"&idt="+typeConcept.getVocabulary().getExternalVocabularyId());
 
+                }
+                else {
+                    this.actionUnitErrorMessage = "No action unit ID specified";
+                }
             }
-            else {
-                this.actionUnitErrorMessage = "No action unit ID specified";
+            catch (RuntimeException e) {
+                this.actionUnitErrorMessage = "Failed to load action unit: " + e.getMessage();
             }
+
         }
-        catch (RuntimeException e) {
-            this.actionUnitErrorMessage = "Failed to load action unit: " + e.getMessage();
-        }
+
 
     }
 
