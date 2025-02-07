01add2f10bbdbb827b47d519d3718592701a9c8b
Julien Linget <julien.linget@mom.fr>
2025-02-07 14:40:58 +0100

    test: FieldServiceTest.java

---
+++ src/test/java/fr/siamois/services/vocabulary/FieldServiceTest.java
@@ -0,0 +1,29 @@
+package fr.siamois.services.vocabulary;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class FieldServiceTest {
+
+    private FieldService fieldService;
+
+    @BeforeEach
+    void setUp() {
+        fieldService = new FieldService();
+    }
+
+    @Test
+    void searchAllFieldCodes() {
+        List<String> fieldCodes = fieldService.searchAllFieldCodes();
+
+        assertThat(fieldCodes).isNotEmpty();
+    }
+
+}
