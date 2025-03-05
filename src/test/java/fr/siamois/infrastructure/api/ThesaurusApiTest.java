bcc8f0e5a6d907996dec117aba8eb53e8743df3f
Julien Linget <julien.linget@mom.fr>
2025-03-05 16:02:16 +0100

    refactor: Optimize imports

--- src/test/java/fr/siamois/infrastructure/api/ThesaurusApiTest.java
+++ src/test/java/fr/siamois/infrastructure/api/ThesaurusApiTest.java
@@ -8,13 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
