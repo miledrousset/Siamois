3ff6ef3eeab7107b8c44c15ba2c5b210d3374421
Julien Linget <julien.linget@mom.fr>
2025-03-05 11:23:06 +0100

    refactor: Delete unused exception

--- src/main/java/fr/siamois/domain/models/exceptions/institution/NoInstitutionSelectedException.java
+++
@@ -1,7 +0,0 @@
-package fr.siamois.domain.models.exceptions.institution;
-
-public class NoInstitutionSelectedException extends IllegalStateException {
-    public NoInstitutionSelectedException(String s) {
-        super(s);
-    }
-}
