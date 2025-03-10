24278594209f24e78bb4400b49cc84d46036509e
Julien Linget <julien.linget@mom.fr>
2025-03-10 15:47:09 +0100

    test: PersonServiceTest.java

--- src/test/java/fr/siamois/domain/services/person/PersonServiceTest.java
+++ src/test/java/fr/siamois/domain/services/person/PersonServiceTest.java
@@ -1,45 +1,108 @@
 package fr.siamois.domain.services.person;
 
 import fr.siamois.domain.models.auth.Person;
+import fr.siamois.domain.models.exceptions.auth.*;
+import fr.siamois.domain.services.person.verifier.PasswordVerifier;
+import fr.siamois.domain.services.person.verifier.PersonDataVerifier;
 import fr.siamois.infrastructure.repositories.auth.PersonRepository;
+import fr.siamois.infrastructure.repositories.auth.TeamRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
 
     @Mock
+    private TeamRepository teamRepository;
+
+    @Mock
     private PersonRepository personRepository;
 
-    @InjectMocks
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Mock
+    private PasswordVerifier passwordVerifier;
+
     private PersonService personService;
 
-    Person p ;
+    Person person;
 
     @BeforeEach
     void setUp() {
-        p = new Person();
+        person = new Person();
+        person.setId(1L);
+        person.setPassword("password");
+
+        List<PersonDataVerifier> verifiers = List.of(passwordVerifier);
+
+        personService = new PersonService(
+                teamRepository,
+                personRepository,
+                passwordEncoder,
+                verifiers
+        );
     }
 
     @Test
     void findAllByNameLastnameContaining_Success() {
-
-        when(personRepository.findAllByNameOrLastname("bob")).thenReturn(List.of(p));
+        when(personRepository.findAllByNameOrLastname("bob")).thenReturn(List.of(person));
 
         // Act
         List<Person> actualResult = personService.findAllByNameLastnameContaining("bob");
 
         // Assert
-        assertEquals(List.of(p), actualResult);
+        assertEquals(List.of(person), actualResult);
+    }
+
+    @Test
+    void updatePerson_Success() throws UserAlreadyExistException, InvalidNameException, InvalidPasswordException, InvalidUsernameException, InvalidEmailException {
+        // Arrange
+        when(personRepository.save(person)).thenReturn(person);
+
+        // Act
+        personService.updatePerson(person);
+
+        // Assert
+        verify(personRepository, times(1)).save(person);
+    }
+
+    @Test
+    void passwordMatch_Success() {
+        // Arrange
+        when(passwordEncoder.matches("plainPassword", "encodedPassword")).thenReturn(true);
+        person.setPassword("encodedPassword");
+
+        // Act
+        boolean result = personService.passwordMatch(person, "plainPassword");
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    void updatePassword_Success() throws InvalidPasswordException {
+        // Arrange
+
+        when(passwordEncoder.encode("newPassword")).thenReturn("encodedNewPassword");
+
+        // Act
+        personService.updatePassword(person, "newPassword");
+
+        // Assert
+        verify(passwordVerifier, times(1)).verify(person);
+        verify(personRepository, times(1)).save(person);
+        assertEquals("encodedNewPassword", person.getPassword());
     }
 
 }
