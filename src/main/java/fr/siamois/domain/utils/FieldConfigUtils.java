8c7e164fee230bc7a83694d15ac4124cdd544795
Julien Linget <julien.linget@mom.fr>
2025-03-17 11:03:57 +0100

    refactor: Remove unused methods

--- src/main/java/fr/siamois/domain/utils/FieldConfigUtils.java
+++ src/main/java/fr/siamois/domain/utils/FieldConfigUtils.java
@@ -11,7 +11,6 @@ import fr.siamois.ui.bean.LangBean;
 import jakarta.faces.application.FacesMessage;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
 import java.util.Optional;
 
 import static fr.siamois.domain.utils.MessageUtils.displayMessage;
@@ -52,8 +51,6 @@ public class FieldConfigUtils {
 
         } catch (NotSiamoisThesaurusException | InvalidEndpointException e) {
             displayErrorMessage(langBean, "Thesaurus is not valid");
-        } catch (IOException e) {
-            displayErrorMessage(langBean, "Error in thesaurus config");
         }
     }
 
