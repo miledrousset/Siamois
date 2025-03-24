14810bbb9617eb2b631473e496ebe898320302e4
Julien Linget <julien.linget@mom.fr>
2025-03-24 11:08:10 +0100

    feat: Add spatial_unit_hierarchy as ManyToMany

--- src/main/java/fr/siamois/domain/models/spatialunit/SpatialUnit.java
+++ src/main/java/fr/siamois/domain/models/spatialunit/SpatialUnit.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.SQLRestriction;
 
+import java.util.List;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
@@ -18,6 +20,17 @@ public class SpatialUnit extends SpatialUnitGeneric implements ArkEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "spatial_unit_id", nullable = false)
     private Long id;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "spatial_hierarchy",
+            joinColumns = @JoinColumn(name = "fk_parent_id"),
+            inverseJoinColumns = @JoinColumn(name = "fk_child_id")
+    )
+    private List<SpatialUnit> parents;
+
+    @ManyToMany(mappedBy = "parents", fetch = FetchType.LAZY)
+    private List<SpatialUnit> childs;
 
     @FieldCode
     public static final String CATEGORY_FIELD_CODE = "SIASU.TYPE";
