44252299f5c7431ce20a14edac7c52a40435c423
Julien Linget <julien.linget@mom.fr>
2025-03-24 11:27:27 +0100

    feat: Add ru_study_document

--- src/main/java/fr/siamois/domain/models/recordingunit/RecordingUnitStudy.java
+++ src/main/java/fr/siamois/domain/models/recordingunit/RecordingUnitStudy.java
@@ -3,9 +3,13 @@ package fr.siamois.domain.models.recordingunit;
 
 import fr.siamois.domain.models.ArkEntity;
 import fr.siamois.domain.models.FieldCode;
+import fr.siamois.domain.models.document.Document;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -17,6 +21,14 @@ public class RecordingUnitStudy extends RecordingUnityStudyParent implements Ark
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "recording_unit_study_id", nullable = false)
     private Long id;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "ru_study_document",
+            joinColumns = @JoinColumn(name = "fk_document_id"),
+            inverseJoinColumns = @JoinColumn(name = "fk_ru_study_id")
+    )
+    private transient Set<Document> documents = new HashSet<>();
 
     @FieldCode
     public static final String TYPE_FIELD = "SIASRU.TYPE";
