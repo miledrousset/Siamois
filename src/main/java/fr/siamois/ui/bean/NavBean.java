11cb71db17c94970a844af50b0deed40158f1391
Julien Linget <julien.linget@mom.fr>
2025-03-31 11:22:06 +0200

    refactor: Remove unused functions

--- src/main/java/fr/siamois/ui/bean/NavBean.java
+++ src/main/java/fr/siamois/ui/bean/NavBean.java
@@ -4,7 +4,6 @@ import fr.siamois.domain.models.Institution;
 import fr.siamois.domain.models.auth.Person;
 import fr.siamois.domain.services.InstitutionService;
 import fr.siamois.domain.services.publisher.InstitutionChangeEventPublisher;
-import fr.siamois.domain.utils.AuthenticatedUserUtils;
 import fr.siamois.ui.bean.converter.InstitutionConverter;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
-import java.util.Optional;
 
 
 /**
@@ -55,40 +53,8 @@ public class NavBean implements Serializable {
         institutions = sessionSettingsBean.getReferencedInstitutions();
     }
 
-    /**
-     * Checks if the user is in the given role
-     * @return true if the user is in the role, false otherwise
-     */
-    public boolean userIsAdmin() {
-        Optional<Person> optUser = AuthenticatedUserUtils.getAuthenticatedUser();
-        return optUser.map(Person::isSuperAdmin).orElse(false);
-    }
-
-    public void changeSelectedInstitution(Institution institution) {
-        Institution old = sessionSettingsBean.getSelectedInstitution();
-        sessionSettingsBean.setSelectedInstitution(institution);
-        institutionChangeEventPublisher.publishInstitutionChangeEvent();
-        log.trace("Institution changed from {} to {}", old.getName(), institution.getName());
-    }
-
-    public boolean institutionRefIsEmpty() {
-        if (institutions == null || institutions.isEmpty()) {
-            institutions = sessionSettingsBean.getReferencedInstitutions();
-        }
-        return institutions.isEmpty();
-    }
-
     public Institution getSelectedInstitution() {
         return sessionSettingsBean.getSelectedInstitution();
-    }
-
-    public boolean isManagerOrAdminOfInstitution() {
-        Optional<Person> optUser = AuthenticatedUserUtils.getAuthenticatedUser();
-        if (optUser.isEmpty())
-            return false;
-        Person person = optUser.get();
-        Institution institution = sessionSettingsBean.getSelectedInstitution();
-        return userIsAdmin() || institutionService.isManagerOf(institution, person);
     }
 
     public void updateInstitutions() {
