9ee2a1d785f4d347f2545d4f6f01edf0eea48cc5
Julien Linget <julien.linget@mom.fr>
2025-04-09 11:16:14 +0200

    feat: Add sidebar check and reduce login page size

--- src/main/resources/META-INF/resources/resources/js/template.js
+++ src/main/resources/META-INF/resources/resources/js/template.js
@@ -2,7 +2,7 @@ const sidebar = document.getElementsByClassName("sidebar")[0];
 const toggleBtn = document.getElementById("toggle-btn");
 const logoutForm = document.getElementById("logout-form");
 
-if (toggleBtn) {
+if (toggleBtn && sidebar) {
     document.addEventListener("DOMContentLoaded", function () {
         toggleBtn.addEventListener("click", function () {
             sidebar.classList.toggle("collapsed");
