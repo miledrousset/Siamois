548d6a2aae48c3c46f5493ddc7670a0cc44c3da1
Julien Linget <julien.linget@mom.fr>
2025-04-14 13:30:23 +0200

    feat: Institution details home page

--- src/main/java/fr/siamois/ui/bean/settings/InstitutionDetailsBean.java
+++ src/main/java/fr/siamois/ui/bean/settings/InstitutionDetailsBean.java
@@ -4,6 +4,7 @@ import fr.siamois.domain.models.Institution;
 import fr.siamois.ui.bean.settings.components.OptionElement;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.faces.bean.SessionScoped;
@@ -11,6 +12,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Getter
 @Setter
 @Component
@@ -22,14 +24,28 @@ public class InstitutionDetailsBean implements Serializable {
 
     public void addInstitutionManagementElements() {
         elements = new ArrayList<>();
-        elements.add(new OptionElement("bi bi-building", "Paramètres de l'organisation", "Nom de l'organisation", () -> redirectTo("")));
-        elements.add(new OptionElement("bi bi-person-circle", "Responsables d'organisation", "Gérer les administrateurs de Bibracte", () -> redirectTo("")));
-        elements.add(new OptionElement("bi bi-people", "Équipes", "Gérer les équipes de Bibracte", () -> redirectTo("")));
-        elements.add(new OptionElement("bi bi-table", "Thesaurus","Configurer le thesaurus utilisé par l'organisation", () -> redirectTo("")));
+        elements.add(new OptionElement("bi bi-building", "Paramètres de l'organisation", "Nom de l'organisation", () -> {
+            log.trace("Clicked on institution settings");
+            return "";
+        }));
+        elements.add(new OptionElement("bi bi-person-circle", "Responsables d'organisation", "Gérer les administrateurs de Bibracte", () -> {
+            log.trace("Clicked on institution managers");
+            return "";
+        }));
+        elements.add(new OptionElement("bi bi-people", "Équipes", "Gérer les équipes de Bibracte", () -> {
+            log.trace("Clicked on institution teams");
+            return "";
+        }));
+        elements.add(new OptionElement("bi bi-table", "Thesaurus","Configurer le thesaurus utilisé par l'organisation", () -> {
+            log.trace("Clicked on thesaurus settings");
+            return "";
+        }));
     }
 
-    public String redirectTo(String pagePath) {
-        return pagePath;
+    public String goToInstitutionList() {
+        institution = null;
+        elements.clear();
+        return "/pages/settings/institutionListSettings.xhtml?faces-redirect=true";
     }
 
 }
