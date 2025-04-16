1cba1a86aa611d2c5eeef61cd72128f810760978
Grégory Bliault <gregorybliault@gmail.com>
2025-04-16 16:18:39 +0200

    Add login event publisher test

---
+++ src/test/java/fr/siamois/domain/services/publisher/LoginEventPublisherTest.java
@@ -0,0 +1,35 @@
+package fr.siamois.domain.services.publisher;
+
+
+import fr.siamois.domain.models.events.LoginEvent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class LoginEventPublisherTest {
+
+    @Mock
+    private ApplicationEventPublisher publisher;
+
+    private LoginEventPublisher loginEventPublisher;
+
+    @BeforeEach
+    void setUp() {
+        loginEventPublisher = new LoginEventPublisher(publisher);
+    }
+
+    @Test
+    void publishLoginEvent_shouldSendChangeEvent() {
+        loginEventPublisher.publishLoginEvent();
+        verify(publisher, times(1)).publishEvent(any(LoginEvent.class));
+    }
+}
