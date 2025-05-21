85b3199d7f225c94c091ed16a2e327e060534df6
Julien Linget <julien.linget@mom.fr>
2025-05-21 16:02:19 +0200

    refactor: Remove unused imports

--- src/test/java/fr/siamois/infrastructure/database/initializer/HistoryTriggerInitializerDiffblueTest.java
+++ src/test/java/fr/siamois/infrastructure/database/initializer/HistoryTriggerInitializerDiffblueTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
 
 import java.sql.*;
 
