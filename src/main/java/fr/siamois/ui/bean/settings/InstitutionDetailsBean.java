e77cf6e901f8075a1ccc1ed7c3afe6f3d915a749
Julien Linget <julien.linget@mom.fr>
2025-06-04 11:22:34 +0200

    feat: Add filter foor action manager view

--- src/main/java/fr/siamois/ui/bean/settings/InstitutionDetailsBean.java
+++ src/main/java/fr/siamois/ui/bean/settings/InstitutionDetailsBean.java
@@ -2,7 +2,9 @@ package fr.siamois.ui.bean.settings;
 
 import fr.siamois.domain.models.events.LoginEvent;
 import fr.siamois.domain.models.institution.Institution;
+import fr.siamois.domain.services.authorization.PermissionService;
 import fr.siamois.ui.bean.LangBean;
+import fr.siamois.ui.bean.SessionSettingsBean;
 import fr.siamois.ui.bean.settings.components.OptionElement;
 import fr.siamois.ui.bean.settings.institution.InstitutionInfoSettingsBean;
 import fr.siamois.ui.bean.settings.institution.InstitutionManagerListBean;
@@ -30,34 +32,44 @@ public class InstitutionDetailsBean implements Serializable {
     private final InstitutionThesaurusSettingsBean institutionThesaurusSettingsBean;
     private final LangBean langBean;
     private final InstitutionActionManagerListBean institutionActionManagerListBean;
+    private final transient PermissionService permissionService;
+    private final SessionSettingsBean sessionSettingsBean;
     private Institution institution;
     private transient List<OptionElement> elements;
 
     public InstitutionDetailsBean(InstitutionInfoSettingsBean institutionInfoSettingsBean,
                                   InstitutionManagerListBean institutionManagerListBean,
                                   InstitutionThesaurusSettingsBean institutionThesaurusSettingsBean,
-                                  LangBean langBean, InstitutionActionManagerListBean institutionActionManagerListBean) {
+                                  LangBean langBean,
+                                  InstitutionActionManagerListBean institutionActionManagerListBean,
+                                  PermissionService permissionService,
+                                  SessionSettingsBean sessionSettingsBean) {
+
         this.institutionInfoSettingsBean = institutionInfoSettingsBean;
         this.institutionManagerListBean = institutionManagerListBean;
         this.institutionThesaurusSettingsBean = institutionThesaurusSettingsBean;
         this.langBean = langBean;
         this.institutionActionManagerListBean = institutionActionManagerListBean;
+        this.permissionService = permissionService;
+        this.sessionSettingsBean = sessionSettingsBean;
     }
 
     public void addInstitutionManagementElements() {
         elements = new ArrayList<>();
 
-        elements.add(new OptionElement("bi bi-building", langBean.msg("organisationSettings.titles.settings"),
-                langBean.msg("organisationSettings.descriptions.settings"), () -> {
-            institutionInfoSettingsBean.init(institution);
-            return "/pages/settings/institution/institutionInfoSettings.xhtml?faces-redirect=true";
-        }));
+        if (permissionService.isInstitutionManager(sessionSettingsBean.getUserInfo())) {
+            elements.add(new OptionElement("bi bi-building", langBean.msg("organisationSettings.titles.settings"),
+                    langBean.msg("organisationSettings.descriptions.settings"), () -> {
+                institutionInfoSettingsBean.init(institution);
+                return "/pages/settings/institution/institutionInfoSettings.xhtml?faces-redirect=true";
+            }));
 
-        elements.add(new OptionElement("bi bi-person-circle", langBean.msg("organisationSettings.titles.managers"),
-                langBean.msg("organisationSettings.descriptions.managers", institution.getName()), () -> {
-            institutionManagerListBean.init(institution);
-            return "/pages/settings/institution/institutionManagerSettings.xhtml?faces-redirect=true";
-        }));
+            elements.add(new OptionElement("bi bi-person-circle", langBean.msg("organisationSettings.titles.managers"),
+                    langBean.msg("organisationSettings.descriptions.managers", institution.getName()), () -> {
+                institutionManagerListBean.init(institution);
+                return "/pages/settings/institution/institutionManagerSettings.xhtml?faces-redirect=true";
+            }));
+        }
 
         // TODO: Add lang support for "Gestionnaires d'actions" descriptions
         elements.add(new OptionElement("bi bi-person-circle",
