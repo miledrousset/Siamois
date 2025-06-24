5fd5091684badf5616107557c9103a27b7c8870c
Julien Linget <julien.linget@mom.fr>
2025-06-24 15:33:23 +0200

    test: Add test to emailVerifier

--- src/test/java/fr/siamois/domain/services/person/verifier/EmailVerifierTest.java
+++ src/test/java/fr/siamois/domain/services/person/verifier/EmailVerifierTest.java
@@ -1,15 +1,21 @@
 package fr.siamois.domain.services.person.verifier;
 
 import fr.siamois.domain.models.auth.Person;
+import fr.siamois.domain.models.exceptions.auth.EmailAlreadyExistException;
 import fr.siamois.domain.models.exceptions.auth.InvalidEmailException;
 import fr.siamois.infrastructure.database.repositories.person.PersonRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EmailVerifierTest {
@@ -20,13 +26,7 @@ class EmailVerifierTest {
     @InjectMocks
     private EmailVerifier emailVerifier;
 
-    @Test
-    void verify_shouldThrowInvalidEmail_whenEmailIsInvalid() {
-        Person person = new Person();
-        person.setEmail("invalid-email");
 
-        assertThrows(InvalidEmailException.class, () -> emailVerifier.verify(person));
-    }
 
     @Test
     void verify_shouldNotThrowException_whenEmailIsValid() throws InvalidEmailException {
@@ -34,5 +34,25 @@ class EmailVerifierTest {
         person.setEmail("valid.email@example.com");
 
         emailVerifier.verify(person);
+    }
+
+    @Test
+    void verify_shouldThrowEmailAlreadyExistException_whenEmailAlreadyExists() {
+        Person person = new Person();
+        person.setEmail("existing.email@example.com");
+
+        when(personRepository.findByEmailIgnoreCase("existing.email@example.com")).thenReturn(Optional.of(person));
+
+        assertThrows(EmailAlreadyExistException.class, () -> emailVerifier.verify(person));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"invalid-email", ""})
+    void verify_shouldThrowInvalidEmailException_forInvalidEmails(String email) {
+        Person person = new Person();
+
+        person.setEmail(email);
+
+        assertThrows(InvalidEmailException.class, () -> emailVerifier.verify(person));
     }
 }
