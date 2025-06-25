41e7e798d1c6de9678371e27b56c700433923645
Julien Linget <julien.linget@mom.fr>
2025-06-25 16:13:05 +0200

    fix: Refresh home panel when Spatial Unit or Action Unit created

--- src/main/java/fr/siamois/ui/bean/panel/FlowBean.java
+++ src/main/java/fr/siamois/ui/bean/panel/FlowBean.java
@@ -336,7 +336,7 @@ public class FlowBean implements Serializable {
 
     public void updateHomePanel() {
         for (AbstractPanel panel : panels) {
-            if (panel instanceof WelcomePanel welcomePanel && Boolean.FALSE.equals(welcomePanel.getCollapsed())) {
+            if (panel instanceof WelcomePanel welcomePanel) {
                 welcomePanel.init();
             }
         }
