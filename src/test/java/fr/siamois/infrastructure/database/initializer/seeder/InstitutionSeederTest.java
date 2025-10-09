279925d788211ea80aeae5683b2ae2b1e155a199
Grégory Bliault <gregorybliault@gmail.com>
2025-10-09 15:30:59 +0200

    Version 0.1

--- src/test/java/fr/siamois/infrastructure/database/initializer/seeder/InstitutionSeederTest.java
+++ src/test/java/fr/siamois/infrastructure/database/initializer/seeder/InstitutionSeederTest.java
@@ -1,7 +1,12 @@
 package fr.siamois.infrastructure.database.initializer.seeder;
 
 import fr.siamois.domain.models.auth.Person;
+import fr.siamois.domain.models.exceptions.ErrorProcessingExpansionException;
+import fr.siamois.domain.models.exceptions.api.NotSiamoisThesaurusException;
+import fr.siamois.domain.models.exceptions.database.DatabaseDataInitException;
 import fr.siamois.domain.models.institution.Institution;
+import fr.siamois.domain.models.vocabulary.Vocabulary;
+import fr.siamois.domain.services.vocabulary.FieldConfigurationService;
 import fr.siamois.infrastructure.database.repositories.institution.InstitutionRepository;
 
 import org.junit.jupiter.api.Test;
@@ -28,25 +33,54 @@ class InstitutionSeederTest {
     @Mock
     PersonSeeder personSeeder;
 
+    @Mock
+    ThesaurusSeeder thesaurusSeeder;
+
+    @Mock
+    FieldConfigurationService fieldConfigurationService;
+
     @InjectMocks
     InstitutionSeeder institutionSeeder;
 
-
     @Test
-    void seed_AlreadyExists() {
+    void seed_ThesaurusNotFound()  {
 
         Institution i = new Institution();
         i.setIdentifier("test");
 
         List<InstitutionSeeder.InstitutionSpec> toInsert = List.of(
-                new InstitutionSeeder.InstitutionSpec("Mon institution", "Test", "test", null)
+                new InstitutionSeeder.InstitutionSpec("Mon institution", "Test", "test", null, "https://thesaurus.mom.fr", "th240")
         );
 
+
+        when(thesaurusSeeder.findVocabularyOrReturnNull("https://thesaurus.mom.fr", "th240")).thenReturn(null);
+
+        IllegalArgumentException ex = assertThrows(
+                IllegalArgumentException.class,
+                () -> institutionSeeder.seed(toInsert)
+        );
+
+        assertThat(ex.getMessage()).contains("Invalid thesaurus: th240" );
+    }
+
+    @Test
+    void seed_AlreadyExists() throws DatabaseDataInitException, NotSiamoisThesaurusException, ErrorProcessingExpansionException {
+
+        Institution i = new Institution();
+        i.setIdentifier("test");
+
+        List<InstitutionSeeder.InstitutionSpec> toInsert = List.of(
+                new InstitutionSeeder.InstitutionSpec("Mon institution", "Test", "test", null, "https://thesaurus.mom.fr", "th240")
+        );
+
+        doReturn(null).when(fieldConfigurationService).setupFieldConfigurationForInstitution(any(Institution.class), any(Vocabulary.class));
+        when(thesaurusSeeder.findVocabularyOrReturnNull("https://thesaurus.mom.fr", "th240")).thenReturn(new Vocabulary());
         when(institutionRepository.findInstitutionByIdentifier(anyString())).thenReturn(Optional.of(i));
 
         institutionSeeder.seed(toInsert);
 
         verify(institutionRepository, never()).save(any(Institution.class));
+        verify(fieldConfigurationService, times(1)).setupFieldConfigurationForInstitution(any(Institution.class), any(Vocabulary.class));
     }
 
     @Test
@@ -57,7 +91,7 @@ class InstitutionSeederTest {
 
         List<InstitutionSeeder.InstitutionSpec> toInsert = List.of(
                 new InstitutionSeeder.InstitutionSpec("Mon institution", "Test", "test",
-                        List.of("user@siamois.fr"))
+                        List.of("user@siamois.fr"),"https://thesaurus.mom.fr", "th240")
         );
 
         when(personSeeder.findPersonOrReturnNull(anyString())).thenReturn(null);
@@ -73,7 +107,7 @@ class InstitutionSeederTest {
     }
 
     @Test
-    void seed_success() {
+    void seed_success() throws DatabaseDataInitException, NotSiamoisThesaurusException, ErrorProcessingExpansionException {
 
         Institution i = new Institution();
         Person p = new Person();
@@ -81,15 +115,19 @@ class InstitutionSeederTest {
 
         List<InstitutionSeeder.InstitutionSpec> toInsert = List.of(
                 new InstitutionSeeder.InstitutionSpec("Mon institution", "Test", "test",
-                        List.of("user@siamois.fr"))
+                        List.of("user@siamois.fr"),"https://thesaurus.mom.fr", "th240")
         );
 
+        doReturn(null).when(fieldConfigurationService).setupFieldConfigurationForInstitution(any(Institution.class), any(Vocabulary.class));
+        when(thesaurusSeeder.findVocabularyOrReturnNull("https://thesaurus.mom.fr", "th240")).thenReturn(new Vocabulary());
         when(personSeeder.findPersonOrReturnNull(anyString())).thenReturn(p);
         when(institutionRepository.findInstitutionByIdentifier(anyString())).thenReturn(Optional.empty());
+        when(institutionRepository.save(any(Institution.class))).thenReturn(new Institution());
 
         institutionSeeder.seed(toInsert);
 
         verify(institutionRepository, times(1)).save(any(Institution.class));
+        verify(fieldConfigurationService, times(1)).setupFieldConfigurationForInstitution(any(Institution.class), any(Vocabulary.class));
 
     }
 }
